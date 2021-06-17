d8a5d6d4501bb0e15e1a89d19d54d7b3a393225b
Erik <43432787+ErikLager@users.noreply.github.com>
2021-06-17 15:00:13 +0200

    Comments

--- helpers.js
+++ helpers.js
@@ -1,3 +1,8 @@
+/**
+ * This replaces blankspaces with '%20' for the fetch to be able to read it
+ * @param {song} str wake me up
+ * @returns wake%20me%20up
+ */
 function prepareQuery(str) {
     for (let i = 0; i < str.length; i++) {
         str = str.replace(" ", "%20");
@@ -5,6 +10,9 @@ function prepareQuery(str) {
     return str;
 }
 
+/**
+ * Replaces \n with <br> so the formating on the site is correct
+ */
 function formatResponse(resp) {
     for (let i = 0; i < resp.length; i++) {
         resp = resp.replace("\n", "<br>");
@@ -13,8 +21,10 @@ function formatResponse(resp) {
 }
 
 function verifylen(event) {
-    event.preventDefault();
-    let checker = 0;
+    event.preventDefault(); //prevents the site from reloading when formbutton is clicked
+    let checker = 0;        //A variable to check if everything is done correctly
+    
+    // If one of the fields are empty a error message is displayed and checker is increased by one
     if (artistfield.value == ""){
         document.getElementById("error1").style.display="block";
         checker++;
@@ -23,6 +33,9 @@ function verifylen(event) {
         document.getElementById("error2").style.display="block";
         checker++;
     }
+
+    // If checker is equal to 0 means both fields have text in them
+    // Then the error messages are removed and the method fetchsong() is called
     if (checker == 0){
         document.getElementById("error1").style.display="none";
         document.getElementById("error2").style.display="none";
--- main.js
+++ main.js
@@ -1,40 +1,50 @@
-let songfield = document.getElementById("song");
-let artistfield = document.getElementById("artist");
-let search = document.getElementById("search");
-let content = document.querySelector(".content")
-
+// The main method, it sends a request to the api with the artist and song
 async function fetchsong() {
+
+    // Writes out on site 'searching...' to make it more dynamic
     textarea.innerHTML = "Searching... This might take a while";
+
+    // Reads what the users has written in the fields and saves the value
     artist = prepareQuery(artistfield.value);
     song = prepareQuery(songfield.value);
 
+    // The url variable with the values in it
     let url = `http://ianertson.com:3500/${artist}/${song}`;
-    await fetch(url).then(function (response) {
-        if (response.ok){ 
-            response.json().then(function (data) {
-                textarea.innerHTML = ""; 
-                if (data == ""){
+    await fetch(url).then(function (response) {             //Sends a request to the api 
+        if (response.ok){                                   //Checks if the api is online
+            response.json().then(function (data) {          //Converts the response to json
+                textarea.innerHTML = "";                    //Clear the textarea
+                if (data == ""){                            //If the response is empty, the song doesn't exist/typo
                     textarea.innerHTML = 
                     `<h1>You made a typo haha</h1>
                     <p>or the song doesn't exist in the api</p>`
                 }else{
-                    textarea.innerHTML = formatResponse(data[0].lyrics); 
+                    textarea.innerHTML = formatResponse(data[0].lyrics); //Calls on formatResponse and inserts the return value in textarea
                 } 
             }) 
         } 
-    }).catch(rejected => {
+    }).catch(rejected => {              //If the fetch failed, status code == 4xx/5xx (error)
             textarea.innerHTML = `${rejected}
         Failed to fetch data. Try again later
-        the api might be down for maintinance 
-        or you misstyped the artist or the song
-        but if you want, check the console for more info (anvanced users only)`
+        the api might be down for maintinance
+        if you want, check the console for more info (anvanced users only)`
         })
 }
 
-let textarea = document.createElement("div");
-textarea.classList.add("lyric")
-content.appendChild(textarea);
-textarea.innerHTML =
+
+
+/* declars the textfields, searchbutton & the <div class="content"> as variables for the js-code to read */
+let songfield = document.getElementById("song");
+let artistfield = document.getElementById("artist");
+let search = document.getElementById("search");
+let content = document.querySelector(".content");
+
+
+
+let textarea = document.createElement("div");   //Creates the textarea under the form
+textarea.classList.add("lyric");                //Add the class lyric to <div class="lyric">
+content.appendChild(textarea);                  //textarea is now a child of <div class="content">
+textarea.innerHTML =                            //Sets default text to the textfield
 `
 <h1>What is this site?</h1>
 <p>
@@ -45,4 +55,4 @@ textarea.innerHTML =
 </p>
 `
 
-search.addEventListener("click", verifylen);
+search.addEventListener("click", verifylen);    //Adds a event listener to the button, making searching possible
